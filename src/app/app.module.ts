b6110902ffd8ce005b5831e4e854bae8af93b2d8
Rodrigo Perez <rpelossi@mobydigital.com>
2022-05-22 20:11:22 -0300

    formulario

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  registrationForm: FormGroup;
+  inputPassType: boolean = false;
+  inputCheckType: boolean = false;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      lastname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+      birthday: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+      checkpass: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]]
+    },
+      // { validator: this.checkPassword }
+    );
+  }
+
+  ngOnInit(): void { }
+
+  register() {
+    console.log(this.registrationForm.value)
+    this.registrationForm.reset()
+  }
+
+  // checkPassword(group: FormGroup): any {
+  //   const pass = group.controls.password?.value
+  //   const checkpass = group.controls.checkpass?.value
+  //   return pass === checkpass ? null : { notSame: true }
+  // }
+
+  toggleInputPassType() {
+    this.inputPassType = !this.inputPassType;
+  }
+
+  toggleInputCheckType() {
+    this.inputCheckType = !this.inputCheckType;
+  }
+
+}
